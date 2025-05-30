921d7c5b841da31550ed578b743046e0ca64ce4d
ssergej97 <sergejsubota1997@gmail.com>
2025-05-30 17:00:54 +0300

    hw-6

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 'use strict';
 
-const userName= prompt("What is your name?");
-const greetings = confirm("Do you want to see greeting?")
-if (greetings) {
-    alert(`Hello ${userName}!`);
-}
+// user name
+const userName = `Sergey`;
+// user age
+const age = 27;
+// is verified in a system?
+let isVerified = true;
+// date of registration
+const dateReg= `30.05.2025`;
+// number of user card
+const numOfUserCard = 10n;
+// has the user have promo code?
+let promoCode = `немає`;
+// user's reserve name
+let reserveName;
+// number of user's bonuses
+let numOfBonus = 0;
+console.log(`Користувач ${userName}, вік ${age}, має бонусів: ${numOfBonus}`);
+console.log(`Верифікація: ${isVerified}. Промокод: ${promoCode}`);
+console.log(`Зареєстрований ${dateReg} з номером картки: ${numOfUserCard}`);
