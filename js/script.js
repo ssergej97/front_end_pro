ffc0eb8bf3b6c7d63bd979221b925c309e3b169b
ssergej97 <sergejsubota1997@gmail.com>
2025-06-05 15:21:42 +0300

    hw-13

--- js/script.js
+++ js/script.js
@@ -1,34 +1,8 @@
 'use strict';
 
-const userName = prompt(`What is your name?`);
-const userAge = Number(prompt(`What is your age?`));
+const numOfHours = +prompt(`Enter a number of hours you want`);
 
-let userNotificationsVerified = confirm(`Are you want ot get notifications?`);
+const numOfSecondsInHours = numOfHours * 3600;
 
-const userId = 1234567890123456789n;
+alert(`In your hours ${numOfSecondsInHours} seconds!`);
 
-let lastLogin = null;
-
-let nickname;
-
-const favouriteTech = [`HTML`, `CSS`, `JavaScript`];
-
-const settings = {
-    theme: `light`,
-    autologin: false,
-}
-
-const sessions = {
-    name: userName,
-    age: userAge,
-    notifications: userNotificationsVerified,
-    id: userId,
-    lastLogin: lastLogin,
-    nickname: nickname,
-    favouriteTech: favouriteTech,
-    settings: settings,
-};
-
-alert(`Привіт, ${sessions.name}! Ваш ID: ${sessions.id}`);
-console.log(sessions);
-console.log(`name: ${typeof sessions.name}`, `age: ${typeof sessions.age}`, `notifications: ${typeof sessions.notifications}`, `id: ${typeof sessions.id}`, `lastLogin: ${typeof sessions.lastLogin}`, `nickname: ${typeof sessions.nickname}`, `favouriteTech: ${typeof sessions.favouriteTech}`, `settings: ${typeof sessions.settings}`);
