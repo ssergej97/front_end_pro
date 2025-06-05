c411df7a34bea135dd5af8e3980ab8fd360ff9c9
ssergej97 <sergejsubota1997@gmail.com>
2025-06-05 15:29:20 +0300

    hw-14

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
-let firstNumber = +prompt(`Enter first number`);
-let secondNumber = +prompt(`Enter second number`);
-let thirdNumber = +prompt(`Enter third number`);
+const firstNumber = +prompt(`Enter first number`);
+const secondNumber = +prompt(`Enter second number`);
+const thirdNumber = +prompt(`Enter third number`);
 
 
-let averageNumber = (firstNumber + secondNumber + thirdNumber) / 3;
+const averageNumber = (firstNumber + secondNumber + thirdNumber) / 3;
 
 alert(`Average number is ${averageNumber}!`);
